e4700a62b62538d1f27cdfc361a2cd11b32e37ef
Luis Valencia <lvalencia@inversionesdrc.com>
2020-11-06 15:49:04 -0400

    Agregando imagenes a los productos

--- facilito_store/urls.py
+++ facilito_store/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from . import views
 
 from products.views import ProductListView
@@ -16,3 +19,6 @@ urlpatterns = [
     path('productos/', include('products.urls')),
     #productos/:id
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
@@ -9,6 +11,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0) 
     slug = models.SlugField(null=False, blank=False, unique=True)
+    image = models.ImageField(upload_to='products/', null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     # def save(self, *args, **kwargs):
@@ -19,6 +22,14 @@ class Product(models.Model):
         return self.title
     
 def set_slug(sender, instance, *args, **kwargs): #Callback
-    instance.slug = slugify(instance.title)
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.title, str(uuid.uuid4())[:8] )
+            )
+
+        instance.slug = slug
 
 pre_save.connect(set_slug, sender=Product)
