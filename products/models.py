3df372e0ff1fe4a8883701ce518a6b5310d84d9a
Luis Valencia <lvalencia@inversionesdrc.com>
2020-11-06 10:18:40 -0400

    Agregando pre_save

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
@@ -10,10 +11,14 @@ class Product(models.Model):
     slug = models.SlugField(null=False, blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Product, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
     
+def set_slug(sender, instance, *args, **kwargs): #Callback
+    instance.slug = slugify(instance.title)
+
+pre_save.connect(set_slug, sender=Product)
